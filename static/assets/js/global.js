785c123e1522b9348b08b37ace18f26c14c48166
stichtingvoelspriet <fjtdg+stichtingvoelspriet@hotmail.com>
2018-12-30 10:32:59 +0100

    Remove duplicate unused files

--- static/assets/js/global.js
+++
@@ -1,9 +0,0 @@
-var M = (function (m) {
-  'use strict';
-  $('#logo + .title').click(function () {
-    m.goTo(m.jwin.innerHeight());
-  });
-  m.jwin = $(window);
-  m.callDiv = $('.title:first');
-  return m;
-})(M || {});
